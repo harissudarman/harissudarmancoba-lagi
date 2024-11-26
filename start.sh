69532f7e6b98f0ea110e72378befa3b16ad56468
haris sudarman <Harissudarman28@gmail.com>
2024-11-26 16:05:44 +0700

    start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 streamlit run app.py --server.port 8501 --server.address 0.0.0.0
 
 # Jalankan Flask API di background pada port 10000
-gunicorn endpoint:app --bind 0.0.0.0:10000 &
+gunicorn endpoint:app --bind 0.0.0.0:10000 
 
