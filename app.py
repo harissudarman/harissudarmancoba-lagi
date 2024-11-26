fb6ff6e56f73042928f546c0fdf19a9a60ee7776
haris sudarman <Harissudarman28@gmail.com>
2024-11-26 18:58:13 +0700

    app.py

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def combined_similarity(text1, text2):
     return levenshtein_score * 100  # Kembalikan dalam persen
 
 def fetch_pdf_links():
-    FLASK_URL = " https://76708-118-136-156-195.ngrok-free.app/api/data"  # URL API
+    FLASK_URL = "https://harissudarmancoba-lagi.onrender.com/get_pdf_links"  # URL API
     try:
         response = requests.get(FLASK_URL)  # Kirim permintaan GET ke API
         response.raise_for_status()  # Periksa apakah respons sukses (status 200)
