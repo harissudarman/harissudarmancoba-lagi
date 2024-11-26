0ff897c5360b48aa9e33861971454c22c4db8015
haris sudarman <Harissudarman28@gmail.com>
2024-11-26 16:37:08 +0700

    download_nltk.py

--- download_nltk.py
+++ download_nltk.py
@@ -1,7 +1,11 @@
-<<<<<<< HEAD
-import nltk
-nltk.download('punkt_tab')
-=======
-import nltk
-nltk.download('punkt_tab')
->>>>>>> c627ed8 (Add start.sh)
+<import nltk
+import os
+
+# Tentukan lokasi unduhan nltk_data
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+
+# Buat folder nltk_data jika belum ada
+os.makedirs(nltk_data_path, exist_ok=True)
+
+# Unduh dataset 'punkt'
+nltk.download('punkt', download_dir=nltk_data_path)
