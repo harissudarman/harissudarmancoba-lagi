3cd0bf9452e2d9f62ca91c7aae138d6fd2093b87
haris sudarman <Harissudarman28@gmail.com>
2024-11-26 17:30:36 +0700

    download_nltk.py

--- download_nltk.py
+++ download_nltk.py
@@ -8,4 +8,4 @@ nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
 os.makedirs(nltk_data_path, exist_ok=True)
 
 # Unduh dataset 'punkt'
-nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('punkt_tab', download_dir=nltk_data_path)
