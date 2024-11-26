24ab305083bce210089a752668c8eb95402ca1a0
harissudarman <Harissudarman28@gmail.com>
2024-11-26 13:39:54 +0700

    endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,12 +1,9 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-@app.route('/', methods=['GET'])
-def home():
-    return "https://harissudarmancoba-lagi.onrender.com/get_pdf_links" 
+
 @app.route('/get_pdf_links', methods=['GET'])
-def  home get_pdf_links():
-    
+def get_pdf_links():
     pdf_links = {
         "pdf_links": [
             "https://eprints.unmer.ac.id/id/eprint/282/1/JURNAL%20PENELITIAN%20PENDIDIKAN.pdf",
@@ -14,13 +11,11 @@ def  home get_pdf_links():
             "https://drive.google.com/uc?export=download&id=1FhVv9nDSwha6kbOyqFb46-5bnTHkCKRK",
             "https://journal.uinjkt.ac.id/index.php/ti/article/view/27927/pdf",
             "https://journal.uinjkt.ac.id/index.php/ti/article/view/28585/pdf",
-           
+
             # Tautan terpercaya open source
-            "https://arxiv.org/pdf/2102.01830.pdf",   # Arxiv - Open access paper           
+            "https://arxiv.org/pdf/2102.01830.pdf",   # Arxiv - Open access paper
             "https://journals.plos.org/plosone/article/file?id=10.1371/journal.pone.0231234&type=printable",  # PLOS ONE
-            
-        
-        
+
             # Repositori Universitas Airlangga
             "https://repository.unair.ac.id/14315/1/Literasi_Digital_Remaja_Di_Kota_Surabaya.pdf",  # Literasi Digital Remaja di Surabaya
             "https://repository.unair.ac.id/14235/1/Hubungan_Antara_Moral_Disengagement_Dan_Perilaku_Cyberaggression.pdf",  # Cyberaggression pada Remaja
@@ -31,8 +26,7 @@ def  home get_pdf_links():
             "https://etd.repository.ugm.ac.id/22897/Implementasi_Smart_City.pdf"  # Implementasi Smart City di Kota Semarang
         ]
     }
-    
-    return  jsonify(pdf_links)
+    return jsonify(pdf_links)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
